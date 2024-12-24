46f60db528645a93c4036a47b77c4c4305828bde
MrTypeError <sanugta8@gmail.com>
2024-12-24 22:29:15 +0530

    Opp's Code for the Coffee Machine

--- Coffee-Machine-OOP's/main.py
+++ Coffee-Machine-OOP's/main.py
@@ -1,3 +1,26 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+
+coffee_maker = CoffeeMaker()
+money_maker = MoneyMachine()
+menu = Menu()
+
+
+is_on = True
+while is_on :
+    option = menu.get_items()
+    choice = input(f"Enter The choice form the {option}: ")
+    if choice == "off":
+        is_on = False
+        break
+    elif choice == "report":
+        coffee_maker.report()
+        money_maker.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_maker.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
