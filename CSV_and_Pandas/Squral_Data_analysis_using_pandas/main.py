1d38b3b88b335872478b9897eea9bab1ef6a84f1
MrTypeError <sanugta8@gmail.com>
2024-12-25 16:22:22 +0530

    Analysing the data with Second Approach

--- CSV_and_Pandas/Squral_Data_analysis_using_pandas/main.py
+++ CSV_and_Pandas/Squral_Data_analysis_using_pandas/main.py
@@ -19,7 +19,20 @@ counter_data = {
     "Count" : [gray , cinimon , black]
 }
 
+#Second Approach
 
+data = pd.read_csv("Python-Projects/CSV_and_Pandas/Squral_Data_analysis_using_pandas/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+far_color = data["Primary Fur Color"].to_list()
 
+gray , black , cinimon = 0 , 0 , 0
+
+gray = len(data[data["Primary Fur Color"] == "Gray"])
+cinimon = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black = len(data[data["Primary Fur Color"] == "Black"])
+
+counter_data = {
+    "Far Color" : ["Gray" , "Cinnamon" , "Black"],
+    "Count" : [gray , cinimon , black]
+}
 new_data = pd.DataFrame(counter_data)
 new_data.to_csv("Python-Projects/CSV_and_Pandas/Squral_Data_analysis_using_pandas/Fur_Color_Counter.csv")
